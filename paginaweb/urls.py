c29508b328b1a67492ba67e7e3a94ea2bc088d07
Felipe Meza <fel.meza@duocuc.cl>
2023-06-27 02:32:43 -0400

    Multitud de cambios que no se pueden listar

--- paginaweb/urls.py
+++ paginaweb/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('galeria/', views.galeria, name='galeria'),
     path('formulario/', views.formulario, name='formulario'),
-    path('localizacion', views.localizacion, name='localizacion'),
-    path('mision', views.mision, name='mision'),
+    path('localizacion/', views.localizacion, name='localizacion'),
+    path('mision/', views.mision, name='mision'),
+    path('logout/', exit, name='exit'),
 ]
