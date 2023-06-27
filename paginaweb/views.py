84e2b5e58c4e79a233442a7959124c136a1ca6fc
Felipe Meza <fel.meza@duocuc.cl>
2023-06-27 00:37:01 -0400

    Limpieza y adicion de algunas vistas

--- paginaweb/views.py
+++ paginaweb/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'paginaweb/index.html')
+
+def mision(request):
+    return render(request, 'paginaweb/mision.html')
+
